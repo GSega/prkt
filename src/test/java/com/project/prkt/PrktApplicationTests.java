415e49f1033a58f0336c040329fa10e3bfc84afc
Sergei <gavryndel@ya.ru>
2022-08-23 07:19:03 +0300

    first commit

---
+++ src/test/java/com/project/prkt/PrktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.prkt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
