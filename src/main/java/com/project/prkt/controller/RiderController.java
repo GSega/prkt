a66563e3f8a51b0c939b7816ea8c4ba6578ca477
Sergei <gavryndel@ya.ru>
2022-09-05 14:49:09 +0300

    add show all methods add edit methods

--- src/main/java/com/project/prkt/controller/RiderController.java
+++ src/main/java/com/project/prkt/controller/RiderController.java
@@ -1,23 +1,25 @@
 package com.project.prkt.controller;
 
 import com.project.prkt.model.Rider;
+import com.project.prkt.service.BookingService;
 import com.project.prkt.service.RiderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/client/booking-rider")
+@RequestMapping("/admin/info-riders")
 public class RiderController {
+    BookingService bookingService;
     RiderService riderService;
     @Autowired
-    public RiderController(RiderService riderService) {
+    public RiderController(RiderService riderService, BookingService bookingService) {
         this.riderService = riderService;
+        this.bookingService = bookingService;
     }
+
+
 
     //add new
     @GetMapping("/new-rider")
@@ -27,9 +29,37 @@ public class RiderController {
     }
 
     @PostMapping("/add")
-    public String addNewRiderToDb(@ModelAttribute("newRider") Rider rider){
+    public String addNewRiderToDb(@ModelAttribute("newRider") Rider rider, @RequestParam("id") Long orderId){
         riderService.addNewRiderToDB(rider);
+        //bookingService.addNewRiderToBooking(orderId, rider); uncomment after merge
         return "rider/add_new";  // later change to "redirect: /client/booking-rider";
     }
+
+    //delete
+    @DeleteMapping("/{id}")
+    public void deleteRider(@PathVariable("id") Long id){
+        riderService.deleteRiderById(id);
+    }
+
+    //show all
+    @GetMapping()
+    public String showAllRiders (Model model){
+        model.addAttribute("allRiders", riderService.showAllRiders());
+        model.addAttribute("allBookings", bookingService.showAllBookings());
+        return "rider/show_all";
+    }
+
+    //edit
+    @GetMapping("/edit/{id}")
+    public String showOneRider(@PathVariable("id") Long id, Model model){
+        model.addAttribute("riderToBeUpdated", riderService.showOneRiderById(id));
+        return "rider/edit";
+        }
+    @PatchMapping("/edit/{id}")
+    public String updateRiderById(@PathVariable("id") Long riderToBeUpdatedId,
+                                  @ModelAttribute("oneUpdatedRider") Rider oneUpdatedRider){
+        riderService.updateRiderById(riderToBeUpdatedId, oneUpdatedRider);
+        return "redirect: /admin/info-rider";
+        }
 
 }
--- src/main/java/com/project/prkt/service/RiderService.java
+++ src/main/java/com/project/prkt/service/RiderService.java
@@ -1,19 +1,54 @@
 package com.project.prkt.service;
 
 import com.project.prkt.model.Rider;
+import com.project.prkt.repository.BookingRepository;
 import com.project.prkt.repository.RiderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RiderService {
     private final RiderRepository riderRepository;
+    private final BookingRepository bookingRepository;
     @Autowired
-    public RiderService(RiderRepository riderRepository) {
+    public RiderService(RiderRepository riderRepository, BookingRepository bookingRepository) {
         this.riderRepository = riderRepository;
+        this.bookingRepository = bookingRepository;
     }
 
     public void addNewRiderToDB(Rider rider) {
         riderRepository.save(rider);
     }
+
+    public void deleteRiderById(Long id){
+        riderRepository.deleteById(id);
+    }
+
+    public List<Rider> showAllRiders(){
+        return riderRepository.findAllByOrderById();
+    }
+
+    //edit
+    public Rider showOneRiderById(Long id){
+        return riderRepository.findById(id).orElseThrow(()->
+                new IllegalStateException("Rider with id = " + id + " not found!"));
+    }
+
+    public void updateRiderById(Long riderToBeUpdatedIid, Rider updatedRider){
+        Rider riderToBeUpdated = showOneRiderById(riderToBeUpdatedIid);
+
+        riderToBeUpdated.setName(updatedRider.getName());
+        riderToBeUpdated.setFoot(updatedRider.getFoot());
+        riderToBeUpdated.setSex(updatedRider.getSex());
+        riderToBeUpdated.setHeight(updatedRider.getHeight());
+        riderToBeUpdated.setWeight(updatedRider.getWeight());
+        riderToBeUpdated.getEquipmentNeededIds().clear();
+        riderToBeUpdated.setEquipmentNeededIds(updatedRider.getEquipmentNeededIds());
+
+        riderRepository.save(riderToBeUpdated);
+    }
 }
+
+
